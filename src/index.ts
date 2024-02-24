e85c9d7ef3925583ef9248cff170bd18f54171bd
Suman Koirala <sumanrakoirala@gmail.com>
2024-02-24 10:56:52 -0600

    feat: sequilize added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express, { Express, Request, Response } from "express";
-
-
+import 'dotenv/config'
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("this is working");
